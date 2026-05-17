bc3cc269b8f053c8e01b169ce671a42d34ce1c8f
neothenil <727549953@qq.com>
2026-05-17 18:02:54 +0800

    Chapter 14. Chunks of Bytecode

--- src/value.h
+++ src/value.h
@@ -13,3 +13,4 @@ typedef struct {
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
+void printValue(Value value);
