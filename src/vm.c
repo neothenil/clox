1452bcadbb7edbb59bb043ff301eca01e7bc7d53
neothenil <727549953@qq.com>
2026-06-07 20:45:15 +0800

    21. Global variables

--- src/vm.c
+++ src/vm.c
@@ -119,6 +119,16 @@ static InterpretResult run() {
                 pop();
                 break;
             }
+            case OP_SET_GLOBAL: {
+                ObjString* name = READ_STRING();
+                if (tableSet(&vm.globals, name, peek(0))) {
+                    tableDelete(&vm.globals, name);
+                    runtimeError("Undefined variable '%s'.", name->chars);
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                // assignment is an expression which has return value.
+                break;
+            }
             case OP_EQUAL: {
                 Value b = pop();
                 Value a = pop();
